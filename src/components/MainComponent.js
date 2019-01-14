674a316e4c15df596f295c872ae8e599aba0e76f
Francesco Sardone <f.sardonej@gmail.com>
2019-01-14 18:08:06 +0100

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,3 +1,4 @@
+// json-server --watch db.json -d 2000 -p 3001
 // Importing components
 import React, { Component } from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
